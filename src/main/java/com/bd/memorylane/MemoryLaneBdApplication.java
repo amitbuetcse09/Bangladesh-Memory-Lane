83f1d0315b6776e4e245b840e0cc8554a4431aed
Admin Questtag <amitbuetcse09@gmail.com>
2017-08-25 15:32:15 +0600

    first commit with whole project

---
+++ src/main/java/com/bd/memorylane/MemoryLaneBdApplication.java
@@ -0,0 +1,12 @@
+package com.bd.memorylane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryLaneBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryLaneBdApplication.class, args);
+	}
+}
